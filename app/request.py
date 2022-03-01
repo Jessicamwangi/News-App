5e018e6d68a974b64d484b84c46810b314d325d8
Jessica <jessicamwangi933@gmail.com>
2022-03-01 15:58:30 +0300

    added imports and created variables for api key, source url, cat url and assigned them to none

---
+++ app/request.py
@@ -0,0 +1,9 @@
+import urllib.request,json
+from .models import Article, Category, Source , Headlines
+
+# Getting api key
+api_key = None
+# Getting source url
+source_url= None
+# Getting source url
+cat_url= None
