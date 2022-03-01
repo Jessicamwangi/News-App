7b658c2dede8465e68ee1e368a409c344bd6e2fe
Jessica <jessicamwangi933@gmail.com>
2022-03-01 16:18:14 +0300

    created a class devconfig and set it to debug = true and created a variable config options

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ class Config:
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
-
-
-
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -20,3 +17,18 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
