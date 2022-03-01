b7d816081d914e2bf471bbeacc814a4352801650
Jessica <jessicamwangi933@gmail.com>
2022-03-01 15:59:39 +0300

    created a configure request function and stores the api key variables

--- app/request.py
+++ app/request.py
@@ -7,3 +7,9 @@ api_key = None
 source_url= None
 # Getting source url
 cat_url= None
+
+def configure_request(app):
+    global api_key, source_url, cat_url
+    api_key = app.config['NEWS_API_KEY']
+    source_url= app.config['NEWS_API_SOURCE_URL']
+    cat_url=app.config['CAT_API_URL']
